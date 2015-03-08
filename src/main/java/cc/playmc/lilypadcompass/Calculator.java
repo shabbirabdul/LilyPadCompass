9582f37ce5ee66971c6cffbccc58fd97bc40205f
shabbirabdul <sabdul@ncsu.edu>
2015-03-07 20:30:42 -0500

    testing coverage

---
+++ src/main/java/cc/playmc/lilypadcompass/Calculator.java
@@ -0,0 +1,28 @@
+package cc.playmc.lilypadcompass;
+
+public class Calculator {
+
+	public int add(int x, int y){
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+		System.out.println("adidion of two numbers is "+x+y);
+
+		return x+y;
+	}
+}
--- src/test/java/TestLilyPad.java
+++ src/test/java/TestLilyPad.java
@@ -2,11 +2,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import cc.playmc.lilypadcompass.LilyPadCompass;
+import cc.playmc.lilypadcompass.LilyPadCompassCommand;
+
 
 public class TestLilyPad {
 
 	@Test
 	public void test() {
+		LilyPadCompassCommand lilyPadCompassCommand = new LilyPadCompassCommand();
+		lilyPadCompassCommand.
 	}
 
 }
