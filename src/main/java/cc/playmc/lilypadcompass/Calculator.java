aa03aed500df2dbf2fe44709d5dc9dcd00d66abb
shabbirabdul <sabdul@ncsu.edu>
2015-03-09 22:34:52 -0400

    changes to project to improve code coverage

--- src/main/java/cc/playmc/lilypadcompass/Calculator.java
+++ src/main/java/cc/playmc/lilypadcompass/Calculator.java
@@ -4,25 +4,15 @@ public class Calculator {
 
 	public int add(int x, int y){
 		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-		System.out.println("adidion of two numbers is "+x+y);
-
 		return x+y;
 	}
+	 public int subtract(int x, int y){
+		 return x-y;
+		 
+	 }
+	 public int multiply(int x, int y){
+		 return x*y;
+	 }
+	 
+	 
 }
